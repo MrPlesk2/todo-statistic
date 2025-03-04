0a8f167aed838857ff90159028b1674a41a16465
MrPlesk2 <plesk9@mail.ru>
2025-03-04 15:56:50 +0500

    get all todo comments

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const todoComments = []
+
+files.forEach(file => {
+    const matches = [...file.matchAll(/\/\/\s*TODO\s*(.*?)(?=\r?\n|$)/g)];
+    matches.forEach(match => todoComments.push(match[1])); 
+});
+
+
 
 console.log('Please, write your command!');
 readLine(processCommand);
